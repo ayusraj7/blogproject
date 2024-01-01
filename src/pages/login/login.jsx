3b102f14d35ba395b94dd751ae8d1bbf6d8f7f4f
ayusraj7 <107795694+ayusraj7@users.noreply.github.com>
2024-01-01 09:42:57 +0530

    Update login.jsx

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -15,7 +15,7 @@ const Login = () => {
     loginstart();
     try{
       
-      const response=await axios.post('http://localhost:5000/api/auth/login',{
+      const response=await axios.post('https://backend-1ucg.onrender.com/api/auth/login',{
         email,
         password
       })
